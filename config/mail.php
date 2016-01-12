2ce3ad4bac0887766a5f7a4548d2ed10acd04fd1
Andrey Malygin <hello@andrey-malygin.ru>
2016-01-12 10:57:52 +0500

    mail test

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@andrey-malygin.ru', 'name' => 'GetLock'],
 
     /*
     |--------------------------------------------------------------------------
