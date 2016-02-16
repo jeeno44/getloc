e8eb7d00d18147abe0191418bb8509cd9fc2407b
Mio <Mio@imac-sultanov.dlink>
2016-02-16 14:27:52 +0500

    Переделал верстку фронт-енда

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -51,14 +51,26 @@
 @else
     <header class="site__header">
         <div class="site__header-layout">
-            <a href="{{route('main')}}" class="logo">
+            <h1 class="logo anchor" data-href="#up">
                 <img src="/assets/img/logo.png" width="90" height="26" alt="getLoc">
-            </a>
-            <nav class="header__menu">
-                <a href="{{route('main.feature')}}">{{trans('phrases.capabilities')}}</a>
-                <a href="{{route('scan.main')}}" class="active">{{trans('phrases.analytics')}}</a>
-            </nav>
-            <a class="btn btn_header btn_header-login popup__open" data-popup="login">{{trans('phrases.login')}}</a>
+            </h1>
+            <div class="site__header-inner">
+                <nav class="header__menu">
+                    <a class="active" href="#">{{trans('phrases.capabilities')}}</a>
+                    <a href="#">Цены</a>
+                    <a href="#">О проекте</a>
+                </nav>
+                <a class="btn btn_header btn_header-login popup__open" data-popup="order">{{trans('phrases.get_demo')}}</a>
+                <div class="language">
+                    <button class="language__btn">RU</button>
+                    <ul class="language__list">
+                        <li><a href="#">Русский</a></li>
+                        <li><a class="active" href="#">English</a></li>
+                        <li><a href="#">Deutsch</a></li>
+                        <li><a href="#">Español</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </header>
 @endif
