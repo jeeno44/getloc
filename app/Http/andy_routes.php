045bf42ac571debd7e599766316d98646256b44b
Andrey Malygin <hello@andrey-malygin.ru>
2016-02-17 12:06:13 +0500

    facebook fix

--- app/Http/andy_routes.php
+++ app/Http/andy_routes.php
@@ -18,8 +18,10 @@ Route::group(['middleware' => ['web']], function ()  use ($domain){
             Route::any('/google/callback', ['as' => 'google.callback', 'uses' => 'Auth\SocialController@googleCallback']);
             Route::get('/twitter/redirect', ['as' => 'twitter.redirect', 'uses' => 'Auth\SocialController@linkToTwitter']);
             Route::any('/twitter/callback', ['as' => 'twitter.callback', 'uses' => 'Auth\SocialController@twitterCallback']);
-            Route::get('/facebook/redirect', ['as' => 'google.redirect', 'uses' => 'Auth\SocialController@linkToFacebook']);
-            Route::any('/facebook/callback', ['as' => 'google.callback', 'uses' => 'Auth\SocialController@facebookCallback']);
+            Route::get('/facebook/redirect', ['as' => 'facebook.redirect', 'uses' => 'Auth\SocialController@linkToFacebook']);
+            Route::any('/facebook/callback', ['as' => 'facebook.callback', 'uses' => 'Auth\SocialController@facebookCallback']);
+            Route::get('/social/password', ['as' => 'social.password', 'uses' => 'Auth\SocialController@getPassword']);
+            Route::post('/social/password', ['as' => 'social.password.save', 'uses' => 'Auth\SocialController@postPassword']);
         });
     });
 
