525de3b669a4f2ffd732915eb9d2e5f5ec58aa8d
Mio <Mio@imac-sultanov.dlink>
2016-04-07 18:04:18 +0500

    fast fix

--- hungry_storm/textCollector.py
+++ hungry_storm/textCollector.py
@@ -179,7 +179,7 @@ for item in ps.listen():
         urls  = []
 
         data_           = json.loads(item['data'].decode("utf-8"))
-        db              = MySQLdb.connect(host=mysql_credentials['host'], user=mysql_credentials['user'], passwd=mysql_credentials['password'], db=mysql_credentials['db'], charset=mysql_credentials['charset'], socket=mysql_credentials['unix_socket'])
+        db              = MySQLdb.connect(host=mysql_credentials['host'], user=mysql_credentials['user'], passwd=mysql_credentials['password'], db=mysql_credentials['db'], charset=mysql_credentials['charset'], unix_socket=mysql_credentials['unix_socket'])
         trans_client    = 'blackgremlin2'
         trans_secret    = 'SMnjwvLx0bB2u9Cn05K2vkTE1bSkX0+fsLp/23gsytU='
         
