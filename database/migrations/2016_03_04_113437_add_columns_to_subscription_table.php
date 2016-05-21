60bbe41aadf99c8b921cc4d5dac7dcfe42552bde
Andrey Malygin <hello@andrey-malygin.ru>
2016-05-21 15:07:53 +0500

    remove migrate

--- database/migrations/2016_03_04_113437_add_columns_to_subscription_table.php
+++ database/migrations/2016_03_04_113437_add_columns_to_subscription_table.php
@@ -15,6 +15,10 @@ class AddColumnsToSubscriptionTable extends Migration
         Schema::table('subscriptions', function(Blueprint $table) {
             $table->integer('last_id', false, true);
             $table->integer('count_words', false, true);
+            $table->boolean('white_label');
+            $table->integer('count_languages', false, true);
+            $table->integer('site_id', false, true)->nullable();
+            $table->foreign('site_id')->references('id')->on('sites')->onDelete('cascade');
         });
     }
 
