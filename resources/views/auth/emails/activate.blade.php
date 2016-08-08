0b1ee4d46d5319f5c55d097520acacf1c53490db
Andrey Malygin <hello@andrey-malygin.ru>
2016-08-08 13:53:53 +0500

    fix

--- resources/views/auth/emails/activate.blade.php
+++ resources/views/auth/emails/activate.blade.php
@@ -2,7 +2,7 @@
 Ваше имя: {{$user->first_name}}<br>
 Контактный email: {{$user->email}}<br>
 <br>
-Для подтверждение регистрации и защиты от злоумышленников. Необходимо активировать ваш профиль. Для этого перейдите по ссылке: <a href="http://scan.{{env('APP_DOMAIN')}}/activated?code={{$user->activation_code}}"></a><br>
+Для подтверждение регистрации и защиты от злоумышленников. Необходимо активировать ваш профиль. Для этого перейдите по ссылке: <a href="http://scan.{{env('APP_DOMAIN')}}/activated?code={{$user->activation_code}}">http://scan.{{env('APP_DOMAIN')}}/activated?code={{$user->activation_code}}</a><br>
 <br>
 Напоминаем, что в демо-режиме доступны 3 расчета.<br>
 Для дальнейшей работы необходимо заполнить форму регистрации контрагента.<br>
