2b1a80cafd4ca718fbd784df8ed10482039d873f
Andrey Lejnin <l-and@b3v.ru>
2016-08-08 10:42:13 +0300

    fixes

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -1,15 +1,17 @@
 @if (Auth::check())
     @if(strpos(url('/'), 'scan'))
-        @if(\App\UserDetail::where('user_id', Auth::user()->id)->count() > 0)
-            <div class="header__person header__person_scan">
-	            <span>{{\Auth::user()->email}}</span>
-                <ul class="header__person-list">
-                    <li><a href="/profile">{{trans('account.t_my_profile')}}</a></li>
-                    <li><a href="/contragent">{{trans('account.t_contra')}}</a></li>
-                    <li><a href="{{URL::route('logout')}}">{{trans('account.t_exit')}}</a></li>
-                </ul>
-            </div>
-        @endif
+        
+        <div class="header__person header__person_scan">
+            <span>{{\Auth::user()->email}}</span>
+            <ul class="header__person-list">
+                <li><a href="/profile">{{trans('account.t_my_profile')}}</a></li>
+                {{-- @if(\App\UserDetail::where('user_id', Auth::user()->id)->count() > 0) --}}
+                	<li><a href="/contragent">{{trans('account.t_contra')}}</a></li>
+				{{-- @endif --}}
+                <li><a href="{{URL::route('logout')}}">{{trans('account.t_exit')}}</a></li>
+            </ul>
+        </div>
+        
     @else
         <a class="btn btn_header btn_header-login" href="{{route('main.account')}}">{{trans('account.t_main_cabinet')}}</a>
     @endif
